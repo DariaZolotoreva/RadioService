af413a7fa93c9d1877a5c581c8f5cd2e967dc7ec
Daria Zolotoreva <dasha_vid@mail.ru>
2023-07-27 15:59:15 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -0,0 +1,24 @@
+package ru.netology.javaqa;
+
+public class Radio {
+//
+//    public int currentRadioStation;
+//    public int getCurrentRadioStation(){
+//        return currentRadioStation;
+//    }
+//    public void setCurrentRadioStation(int newCurrentRadioStation) {
+//        if (newCurrentRadioStation < 0) {
+//            return;
+//        }
+//        if (newCurrentRadioStation > 9) {
+//            return;
+//        }
+//        currentRadioStation = newCurrentRadioStation;
+//    }
+//
+//    public void increaseVolume() {
+//        if (currentRadioStation < 100) {
+//            currentRadioStation = currentRadioStation + 1;
+//        }
+//    }
+}
